811a65c7796bd7352f1ec7426959fa18ee73dbe4
Vasyl Bevz <v.bevz@developmentaid.org>
2023-06-27 17:05:28 +0300

    first commit

---
+++ src/main/kotlin/com/example/demoSimpleThreePages/DemoSimpleThreePagesApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demoSimpleThreePages
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoSimpleThreePagesApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoSimpleThreePagesApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/demoSimpleThreePages/DemoSimpleThreePagesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.demoSimpleThreePages
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoSimpleThreePagesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
